7db97d0adcf43f2e41af6f8e3113e225364185bb
Lukashevych, Denys <denys.lukashevych@wolterskluwer.com>
2020-03-16 09:26:22 +0200

    +ping route and basic structure of frontend

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Ping from '@/components/Ping';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
+      name: 'Ping',
+      component: Ping,
+    },
+    {
+      path: '/hw',
       name: 'HelloWorld',
       component: HelloWorld,
     },
